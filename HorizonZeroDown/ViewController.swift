843c92cbef8b4881d5795955efa427608c67480c
George <soysmile@outlook.com>
2018-01-25 11:13:14 +0200

    Initial Commit

---
+++ HorizonZeroDown/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HorizonZeroDown
+//
+//  Created by George Heints on 25.01.2018.
+//  Copyright © 2018 George Heints. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
